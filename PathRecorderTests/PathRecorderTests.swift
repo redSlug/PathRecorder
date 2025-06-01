c173ddca0245c4786ca2407a8583ff595c06f7ce
bradley <11279144+redSlug@users.noreply.github.com>
2025-06-01 11:43:55 -0400

    Initial Commit

---
+++ PathRecorderTests/PathRecorderTests.swift
@@ -0,0 +1,17 @@
+//
+//  PathRecorderTests.swift
+//  PathRecorderTests
+//
+//  Created by Brad Dettmer on 6/1/25.
+//
+
+import Testing
+@testable import PathRecorder
+
+struct PathRecorderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
